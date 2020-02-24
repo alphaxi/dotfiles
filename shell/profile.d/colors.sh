d76edbd5566c0ae7a3158aec8cfe7fed255029f7
Alex Ferreira <ferreira.alex.raphael@gmail.com>
2020-02-24 23:12:54 +0100

    Modification of LESS_TERMCAP config for man colors

--- shell/profile.d/colors.sh
+++ shell/profile.d/colors.sh
@@ -12,12 +12,16 @@ alias grep='grep --colors=auto'
 
 
 # less Wrapper function for colors in man pages
-man() {
-	LESS_TERMCAP_md=$'\e[01;31m' \
-	LESS_TERMCAP_me=$'\e[0m' \
-	LESS_TERMCAP_se=$'\e[0m' \
-	LESS_TERMCAP_so=$'\e[01;44;33m' \
-	LESS_TERMCAP_ue=$'\e[0m' \
-	LESS_TERMCAP_us=$'\e[01;32m' \
-	command man "$@"
-}
+export LESS_TERMCAP_mb=$(tput setaf 1)                              # Start blinking
+export LESS_TERMCAP_md=$(tput bold; tput setaf 1)                   # Start bold mode
+export LESS_TERMCAP_us=$(tput smul; tput bold)                      # Start underlining
+export LESS_TERMCAP_so=$(tput bold; tput setaf 7; tput setab 1)     # Start standout mode
+export LESS_TERMCAP_mr=$(tput rev)                                  # Start reverse mode
+export LESS_TERMCAP_mh=$(tput dim)                                  # Start half bright mode
+export LESS_TERMCAP_me=$(tput sgr0)                                 # End all mode like so, us, mb, md, and mr
+export LESS_TERMCAP_se=$(tput rmso; tput sgr0)                      # End standout mode
+export LESS_TERMCAP_ue=$(tput rmul; tput sgr0)                      # End underlining
+export LESS_TERMCAP_ZN=$(tput ssubm)                                # Enter subscript mode
+export LESS_TERMCAP_ZV=$(tput rsubm)                                # End subscript mode
+export LESS_TERMCAP_ZO=$(tput ssupm)                                # Enter superscript mode
+export LESS_TERMCAP_ZW=$(tput rsupm)                                # End superscript mode
